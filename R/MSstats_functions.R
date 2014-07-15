8d1f38f93e7dfb757d4cc98b9bda9602fe6455c4
erik verschueren <erik.verschueren@gmail.com>
2014-07-15 11:47:35 -0700

    heatmap fixes

--- R/MSstats_functions.R
+++ R/MSstats_functions.R
@@ -96,9 +96,9 @@ dataToMSSFormat = function(d){
   tmp
 }
 
-plotHeat = function(mss_F, out_file){
-  heat_data = data.frame(mss_F, names=paste(mss_F$Protein,mss_F$uniprot_id,sep=' | '))
-  heat_data = heat_data[,c('names','Label','log2FC')]
+plotHeat = function(mss_F, out_file, labelOrder=NULL, names='Protein'){
+  heat_data = data.frame(mss_F, names=names)
+  #heat_data = mss_F[,c('uniprot_id','Label','log2FC')]
   heat_data_w = dcast(names ~ Label, data=heat_data, value.var='log2FC')
   #gene_names = uniprot_to_gene_replace(uniprot_ac=heat_data_w$Protein)
   rownames(heat_data_w) = heat_data_w$names
@@ -114,15 +114,21 @@ plotHeat = function(mss_F, out_file){
   colors_pos = colorRampPalette(brewer.pal("Reds",n=extreme_val/bin_size))(signed_bins)
   colors_tot = c(colors_neg, colors_pos)
   
-  pheatmap(heat_data_w, scale="none", cellheight=10, cellwidth=10, file=out_file, color=colors_tot, breaks=seq(from=-extreme_val, to=extreme_val, by=bin_size), cluster_cols=F)
+  if(is.null(labelOrder)){
+    pheatmap(heat_data_w, scale="none", cellheight=10, cellwidth=10, file=out_file, color=colors_tot, breaks=seq(from=-extreme_val, to=extreme_val, by=bin_size), cluster_cols=T)  
+  }else{
+    heat_data_w = heat_data_w[,labelOrder]
+    pheatmap(heat_data_w, scale="none", cellheight=10, cellwidth=10, file=out_file, color=colors_tot, breaks=seq(from=-extreme_val, to=extreme_val, by=bin_size), cluster_cols=F)
+  }
+  
   heat_data_w
 }
 
-significantHits = function(mss_results, labels='*', LFC=2, FDR=0.05){
+significantHits = function(mss_results, labels='*', LFC=c(-2,2), FDR=0.05){
   ## get subset based on labels
   selected_results = mss_results[grep(labels,mss_results$Label), ]
   cat(sprintf('SELECTED LABELS FOR HEATMAP:\t%s\n',paste(unique(selected_results$Label), collapse=',')))
-  significant_proteins = selected_results[(!is.na(selected_results$log2FC) & selected_results$adj_pvalue <= FDR & abs(selected_results$log2FC) >= LFC) , 'Protein']
+  significant_proteins = selected_results[(!is.na(selected_results$log2FC) & selected_results$adj_pvalue <= FDR & (selected_results$log2FC >= LFC[2] | selected_results$log2FC <= LFC[1])) , 'Protein']
   significant_results = selected_results[selected_results$Protein %in% significant_proteins, ]
   return(significant_results)
 }
